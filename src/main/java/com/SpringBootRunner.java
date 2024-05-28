8020afa224fddd61d66053f7af8404f18270a152
Thanapati <deepakthapaliya2019@gmail.com>
2024-05-27 20:04:32 -0500

    this is done in bugfix branch

--- src/main/java/com/SpringBootRunner.java
+++ src/main/java/com/SpringBootRunner.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+//this is done in bugfix branch
 public class SpringBootRunner {
 
 	public static void main(String[] args) {
